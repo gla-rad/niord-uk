0e7f783e083796cb3a1bf7f3e9e4f68070584142
Nikolaos Vastardis <n.vastardis@gmail.com>
2023-06-29 10:41:42 +0100

    Give a couple of tries for reading the import file

--- niord-uk-aton-import/src/main/java/org/niord/importer/aton/batch/AbstractUkAtonImportReader.java
+++ niord-uk-aton-import/src/main/java/org/niord/importer/aton/batch/AbstractUkAtonImportReader.java
@@ -54,7 +54,17 @@ public abstract class AbstractUkAtonImportReader extends AbstractItemHandler {
     public void open(Serializable prevCheckpointInfo) throws Exception {
 
         // Get hold of the data file
-        Path path = batchService.getBatchJobDataFile(jobContext.getInstanceId());
+        int tries = 0;
+        Path path = null;
+        // Try a few times - just to handle larger filed
+        while(path == null && tries < 3) {
+            path = batchService.getBatchJobDataFile(jobContext.getInstanceId());
+            // No luck? Try again...
+            if(path == null) {
+                tries += 1;
+                Thread.sleep(1000);
+            }
+        }
 
         // Parse the header row of the Excel file and build a column index
         rowIterator = parseHeaderRow(path, colIndex, getFields());
